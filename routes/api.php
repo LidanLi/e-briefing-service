5701da2cb7eeffb637a172b636d547b978bd2fcb
Lidan Li <lidan.li@canada.ca>
2019-04-08 11:30:06 -0400

    update api for ELDP course

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,73 @@ use Illuminate\Http\Request;
 Route::get('/trips/{trip}/download', 'Api\DownloadTripPackageController')->middleware('auth:api');
 /*Route::get('/trips/{trip}/download', 'Api\DownloadTripPackageController');*/
 
+Route::get('/publishlist', function () {
+    $trip_list = array(array("trip_id" => 23, "trip_name" => "Block1: Program Kick-Off"), 
+                        array("trip_id" => 15, "trip_name" => "Block2: Getting to Work"),
+                        array("trip_id" => 29, "trip_name" => "Block3: How Washington Works"),
+                        array("trip_id" => 34, "trip_name" => "Block4: Pioneering Innovation"),
+                        array("trip_id" => 40, "trip_name" => "Other Learning Activities")
+                        /*array("trip_id" => 25, "trip_name" => "Block5: The Power of Pluralism")*/);
+    $json = json_encode($trip_list);
+    return $json;
+});
+
+
+Route::get('/publishlistexternal', function () {
+    $trip_list = array(array("trip_id" => 41, "trip_name" => "External Advisory Committee"));
+    $json = json_encode($trip_list);
+    return $json;
+});
+
+Route::get('/publishlistadmin', function () {
+    $trip_list = array(array("trip_id" => 23, "trip_name" => "Block1: Program Kick-Off"), 
+                        array("trip_id" => 15, "trip_name" => "Block2: Getting to Work"),
+                        array("trip_id" => 29, "trip_name" => "Block3: How Washington Works"),
+                        array("trip_id" => 34, "trip_name" => "Block4: Pioneering Innovation"),
+                        array("trip_id" => 25, "trip_name" => "Block5: The Power of Pluralism"),
+                        array("trip_id" => 39, "trip_name" => "Block6: Northern Study Tours"),
+                        array("trip_id" => 27, "trip_name" => "Block7: Leading in the Global Arena"),
+                        array("trip_id" => 28, "trip_name" => "Block8: Leading Into the Next Decade and Closing Ceremony"),
+                        array("trip_id" => 40, "trip_name" => "Other Learning Activities"),
+                        array("trip_id" => 1, "trip_name" => "ELDP Demo")
+                    );
+    $json = json_encode($trip_list);
+    return $json;
+});
+
+Route::get('/publishlistfrench', function () {
+    $trip_list = array(array("trip_id" => 36, "trip_name" => "Bloc 1: Lancement du programme"),
+                        array("trip_id" => 19, "trip_name" => "Bloc 2: Se Mettre au travail"),
+                        array("trip_id" => 30, "trip_name" => "Bloc 3: Les rouages de Washington"),
+                        array("trip_id" => 38, "trip_name" => "Bloc 4: Innovation pionnière"),
+                        array("trip_id" => 43, "trip_name" => "Autres activités d'apprentissage")
+                        /*array("trip_id" => 31, "trip_name" => "Bloc 5: Le pouvoir du pluralisme")*/);
+    $json = json_encode($trip_list);
+    return $json;
+});
+
+Route::get('/publishlistfrenchexternal', function () {
+    $trip_list = array(array("trip_id" => 42, "trip_name" => "Comite consultatif externe"));
+    $json = json_encode($trip_list);
+    return $json;
+});
+
+Route::get('/publishlistfrenchadmin', function () {
+    $trip_list = array(array("trip_id" => 36, "trip_name" => "Bloc 1: Lancement du programme"),
+                        array("trip_id" => 19, "trip_name" => "Bloc 2: Se Mettre au travail"),
+                        array("trip_id" => 30, "trip_name" => "Bloc 3: Les rouages de Washington"),
+                        array("trip_id" => 38, "trip_name" => "Bloc 4: Innovation pionnière"),
+                        array("trip_id" => 31, "trip_name" => "Bloc 5: Le pouvoir du pluralisme"),
+                        array("trip_id" => 35, "trip_name" => "Bloc 6: Tour d'étude du nord"),
+                        array("trip_id" => 32, "trip_name" => "Bloc 7: Le leadership et la mondialisation"),
+                        array("trip_id" => 33, "trip_name" => "Bloc 8: Entrer dans la prochaine décennie"),
+                        array("trip_id" => 43, "trip_name" => "Autres activités d'apprentissage")
+
+                    );
+    $json = json_encode($trip_list);
+    return $json;
+});
+
 Route::get('/events/{event}/participants/available', function (\App\Event $event) {
     return $event->available_participants;
 });
